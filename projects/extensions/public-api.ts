cf8ad28cdd87cd59e6abfcd32b23504c0d4050b7
nzbin <nzb329@163.com>
2019-12-06 09:27:02 +0800

    build: add flat module file name

--- projects/extensions/public-api.ts
+++ projects/extensions/public-api.ts
@@ -2,8 +2,8 @@
  * Public API Surface of components
  */
 
-export * from '@ng-matero/extensions/alert';
 export * from '@ng-matero/extensions/data-grid';
+export * from '@ng-matero/extensions/alert';
 export * from '@ng-matero/extensions/dialog';
 export * from '@ng-matero/extensions/progress';
 
