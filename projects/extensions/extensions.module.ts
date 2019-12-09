4570f62b77b2f549156da4a10d0d983780ea0bb3
nzbin <nzb329@163.com>
2019-12-09 23:29:00 +0800

    refactor: add select module

--- projects/extensions/extensions.module.ts
+++ projects/extensions/extensions.module.ts
@@ -4,8 +4,10 @@ import { MtxAlertModule } from '@ng-matero/extensions/alert';
 import { MtxDataGridModule } from '@ng-matero/extensions/data-grid';
 import { MtxDialogModule } from '@ng-matero/extensions/dialog';
 import { MtxProgressModule } from '@ng-matero/extensions/progress';
+import { MtxSelectModule } from '@ng-matero/extensions/select';
 
 @NgModule({
-  exports: [MtxAlertModule, MtxDataGridModule, MtxDialogModule, MtxProgressModule],
+  exports: [MtxAlertModule, MtxDataGridModule, MtxDialogModule, MtxProgressModule, MtxSelectModule],
+  declarations: [],
 })
-export class MaterialExtensionsModule { }
+export class MaterialExtensionsModule {}
---
+++ projects/extensions/select/select.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'select',
+  templateUrl: './select.component.html',
+  styleUrls: ['./select.component.css']
+})
+export class SelectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
