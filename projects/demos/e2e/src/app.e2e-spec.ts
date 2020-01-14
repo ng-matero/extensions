37c31cd3299fb2c5907a52dfd299b25666a66481
nzbin <nzb329@163.com>
2020-01-14 22:18:42 +0800

    refactor: rename demo to demos

--- projects/demos/e2e/src/app.e2e-spec.ts
+++ projects/demos/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to demo!');
+    expect(page.getTitleText()).toEqual('Welcome to demos!');
   });
 
   afterEach(async () => {
