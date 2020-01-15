0c834986b95529d45f5943299e0d27b6590b7ad2
nzbin <nzb329@163.com>
2020-01-15 22:47:41 +0800

    refactor: add ngx-progressbar

--- projects/demos/src/app/dev-app/dev-app-module.ts
+++ projects/demos/src/app/dev-app/dev-app-module.ts
@@ -1,19 +1,15 @@
-/**
- * @license
- * Copyright Google LLC All Rights Reserved.
- *
- * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://angular.io/license
- */
-
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { NgProgressModule } from '@ngx-progressbar/core';
+import { NgProgressRouterModule } from '@ngx-progressbar/router';
+
 import { DevApp404 } from './dev-app-404';
 import { DevAppHome } from './dev-app-home';
 import { DevAppLayout } from './dev-app-layout';
@@ -27,6 +23,8 @@ import { DevAppLayout } from './dev-app-layout';
     MatListModule,
     MatSidenavModule,
     MatToolbarModule,
+    NgProgressModule,
+    NgProgressRouterModule,
   ],
   declarations: [DevAppLayout, DevAppHome, DevApp404],
   exports: [DevAppLayout],
