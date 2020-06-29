d06f1d83cc2d143c0318f0bc382ef743fea1f20e
nzbin <nzb329@163.com>
2020-06-29 23:07:21 +0800

    refactor: improve docs home page style

--- projects/demos/src/app/docs-app-home.ts
+++ projects/demos/src/app/docs-app-home.ts
@@ -3,41 +3,50 @@ import { DOCS } from './shared/component-nav/component-nav.component';
 
 @Component({
   template: `
-  <div class="docs-component-section" *ngFor="let section of list">
-    <div class="docs-component-section-title">{{section.title}}</div>
-    <div class="docs-component-list" fxLayout="row wrap" fxLayoutGap="32px grid">
-      <div *ngFor="let item of section.children" fxFlex.gt-sm="25" fxFlex.gt-xs="50" fxFlex="100">
-        <a class="docs-component-list-item" [routerLink]="[item.route]">
-          <mat-card>
-            <mat-card-title>
-              <p>{{item.name}}</p>
-            </mat-card-title>
-            <mat-card-content class="docs-component-list-item-summary">
-              <p>{{item.summary}}</p>
-            </mat-card-content>
-          </mat-card>
+  <div class="docs-component-category-section" *ngFor="let section of list">
+    <div class="docs-component-category-section-title">{{section.title}}</div>
+    <div class="docs-component-category-list" fxLayout="row wrap" fxLayoutGap="32px grid">
+      <div *ngFor="let item of section.children" fxFlex.gt-lg="20" fxFlex.gt-md="25" fxFlex.gt-sm="33.333" fxFlex.gt-xs="50" fxFlex="100">
+        <a class="docs-component-category-list-item" [routerLink]="[item.route]">
+          <div class="docs-component-category-list-card">
+            <div class="docs-component-category-list-card-title">{{item.name}}</div>
+            <div class="docs-component-category-list-card-summary">{{item.summary}}</div>
+          </div>
         </a>
       </div>
     </div>
   </div>
   `,
   styles: [`
-  .docs-component-section {
+  .docs-component-category-section {
     margin-bottom: 2rem;
   }
-  .docs-component-section-title {
+  .docs-component-category-section-title {
     margin-bottom: 1rem;
   }
-  .docs-component-list-item {
-    display: block;
-    height: 100%;
+  .docs-component-category-list-item {
     text-decoration: none;
   }
-  mat-card {
+  .docs-component-category-list-card {
     height: 100%;
+    border: 1px solid rgba(0, 0, 0,.12);
+    border-radius: 4px;
+    background: #fff;
   }
-  .docs-component-list-item-summary {
-    color: rgba(0,0,0,.54);
+  .docs-component-category-list-card:hover {
+    background: rgba(0, 0, 0, .03);
+  }
+  .docs-component-category-list-card-title {
+    padding: 15px;
+    font-size: 20px;
+    font-weight: 500;
+    color: rgba(0,0,0,.87);
+  }
+  .docs-component-category-list-card-summary {
+    padding: 0 15px 15px;
+    min-height: 2.4em;
+    font-size: 15px;
+    color: rgba(0, 0, 0,.54);
   }
   `]
 })
--- projects/demos/src/app/shared/component-nav/component-nav.component.ts
+++ projects/demos/src/app/shared/component-nav/component-nav.component.ts
@@ -11,12 +11,12 @@ export const DOCS = [
       },
       {
         name: 'Color Picker',
-        summary: 'Select color with the ngx-color.',
+        summary: 'An extra input to select color enhanced by the ngx-color.',
         route: '/color-picker'
       },
       {
         name: 'Checkbox Group',
-        summary: 'Allows the user to create a set of checkbox.',
+        summary: 'Allows the user to create a set of checkbox with configuration.',
         route: '/checkbox-group'
       },
       {
@@ -26,22 +26,22 @@ export const DOCS = [
       },
       {
         name: 'Dialog',
-        summary: 'A configurable modal that displays dynamic content.',
+        summary: 'A configurable modal to show alert and confirmation.',
         route: '/dialog'
       },
       {
         name: 'Progress',
-        summary: 'A linear progress indicator.',
+        summary: 'A linear progress indicator with Bootstrap style.',
         route: '/progress'
       },
       {
         name: 'Select',
-        summary: 'Form field wrapper for the ng-select.',
+        summary: 'An ng-select wrapper to be used in the form field.',
         route: '/select'
       },
       {
         name: 'Split Pane',
-        summary: 'Easiest way for creating multi-view layouts',
+        summary: 'An component for creating multi-view layouts which cloned from angular-split.',
         route: '/split-pane'
       },
     ],
