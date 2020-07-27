19a487634614780266f8a5bc5165ae237abcc186
Marcos Ribes <mlribes@gmail.com>
2020-07-27 13:43:42 -0300

    Added 'typeParameter' to MtxGridColumn

--- projects/extensions/data-grid/grid.interface.ts
+++ projects/extensions/data-grid/grid.interface.ts
@@ -43,6 +43,14 @@ export interface MtxGridColumn {
   resizable?: boolean;
   sortable?: boolean | string;
   type?: 'tag' | 'button' | 'link' | 'image' | 'number' | 'currency' | 'percent' | 'boolean' | 'date';
+  typeParameter?: {
+    currencyCode?: string;
+    display?: string | boolean;
+    digitsInfo?: string;
+    format?: string;
+    timezone?: string;
+    locale?: string;
+  };
   tag?: MtxGridColumnTag;
   buttons?: MtxGridColumnButton[];
   formatter?: (rowData: any, colDef?: any) => void;
