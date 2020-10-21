a47eabda2165b2b9f898486cbbaef1756bd70ae7
nzbin <nzb329@163.com>
2020-10-21 08:06:30 +0800

    fix(select): fix placeholder and container click issue

--- projects/extensions/select/select.component.ts
+++ projects/extensions/select/select.component.ts
@@ -311,7 +311,13 @@ export class MtxSelectComponent
   }
 
   /** Implemented as part of MatFormFieldControl. */
-  onContainerClick() {}
+  onContainerClick(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+    if (/mat-form-field|mtx-select/g.test(target.parentElement.classList[0])) {
+      this.focus();
+      this.open();
+    }
+  }
 
   /**
    * Sets the select's value. Part of the ControlValueAccessor interface
