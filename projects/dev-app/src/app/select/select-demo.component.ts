86f4677b3192205b5159cc87b15ff83f1e92c5fc
nzbin <nzb329@163.com>
2020-11-19 12:21:55 +0800

    fix(select): fix option template prop

--- projects/dev-app/src/app/select/select-demo.component.ts
+++ projects/dev-app/src/app/select/select-demo.component.ts
@@ -6,6 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./select-demo.component.scss'],
 })
 export class SelectDemoComponent {
+  multipleSelect = false;
+  disableSelect = false;
+  closeOnSelect = true;
+
   items = [
     { id: 1, name: 'Apple' },
     { id: 2, name: 'Lemon' },
@@ -16,8 +20,86 @@ export class SelectDemoComponent {
 
   value = 5;
 
-  multipleSelect = false;
-  disableSelect = false;
+  selectedAccount = 'Adam';
+  accounts = [
+    {
+      name: 'Adam',
+      email: 'adam@email.com',
+      age: 12,
+      country: 'United States',
+      child: { state: 'Active' },
+    },
+    {
+      name: 'Homer',
+      email: 'homer@email.com',
+      age: 47,
+      country: '',
+      child: { state: 'Active' },
+    },
+    {
+      name: 'Samantha',
+      email: 'samantha@email.com',
+      age: 30,
+      country: 'United States',
+      child: { state: 'Active' },
+    },
+    {
+      name: 'Amalie',
+      email: 'amalie@email.com',
+      age: 12,
+      country: 'Argentina',
+      child: { state: 'Active' },
+    },
+    {
+      name: 'Estefanía',
+      email: 'estefania@email.com',
+      age: 21,
+      country: 'Argentina',
+      child: { state: 'Active' },
+    },
+    {
+      name: 'Adrian',
+      email: 'adrian@email.com',
+      age: 21,
+      country: 'Ecuador',
+      child: { state: 'Active' },
+    },
+    {
+      name: 'Wladimir',
+      email: 'wladimir@email.com',
+      age: 30,
+      country: 'Ecuador',
+      child: { state: 'Inactive' },
+    },
+    {
+      name: 'Natasha',
+      email: 'natasha@email.com',
+      age: 54,
+      country: 'Ecuador',
+      child: { state: 'Inactive' },
+    },
+    {
+      name: 'Nicole',
+      email: 'nicole@email.com',
+      age: 43,
+      country: 'Colombia',
+      child: { state: 'Inactive' },
+    },
+    {
+      name: 'Michael',
+      email: 'michael@email.com',
+      age: 15,
+      country: 'Colombia',
+      child: { state: 'Inactive' },
+    },
+    {
+      name: 'Nicolás',
+      email: 'nicole@email.com',
+      age: 43,
+      country: 'Colombia',
+      child: { state: 'Inactive' },
+    },
+  ];
 
   change(e: any) {
     console.log(e);
