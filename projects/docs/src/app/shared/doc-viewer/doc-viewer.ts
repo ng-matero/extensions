3951c1f3562310560b09def8cf8f7b5762ff2ebd
nzbin <nzb329@163.com>
2021-05-06 12:20:16 +0800

    refactor(docs): improve markdown styles

--- projects/docs/src/app/shared/doc-viewer/doc-viewer.ts
+++ projects/docs/src/app/shared/doc-viewer/doc-viewer.ts
@@ -1,9 +1,10 @@
-import { Component, Input, OnDestroy } from '@angular/core';
+import { Component, Input, OnDestroy, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'doc-viewer',
   template: `<div class="docs-markdown" [innerHTML]="textContent"></div>`,
   styleUrls: ['./doc-viewer.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class DocViewer implements OnDestroy {
   /** The document text. It should not be HTML encoded. */
