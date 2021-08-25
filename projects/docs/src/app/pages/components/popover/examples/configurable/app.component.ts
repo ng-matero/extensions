40da7997fa4be519b5648cbf7d7fb51367209844
nzbin <nzb329@163.com>
2021-08-25 22:59:48 +0800

    docs(popover): update demos and API

--- projects/docs/src/app/pages/components/popover/examples/configurable/app.component.ts
+++ projects/docs/src/app/pages/components/popover/examples/configurable/app.component.ts
@@ -1,21 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {
-  MtxPopoverPositionX,
-  MtxPopoverPositionY,
-} from '@ng-matero/extensions/popover/popover-types';
+  MtxPopoverPosition,
+  MtxPopoverPositionEnd,
+  MtxPopoverPositionStart,
+} from '@ng-matero/extensions/popover';
 
 @Component({
   selector: 'loader-example',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  xPosition: MtxPopoverPositionX = 'after';
-  yPosition: MtxPopoverPositionY = 'below';
+export class AppComponent implements OnInit {
   enterDelay = 200;
   leaveDelay = 200;
   xOffset = 0;
   yOffset = 0;
-  overlapTrigger = false;
   closeOnPanelClick = false;
+
+  positionXOptions = ['before', 'after'];
+  positionYOptions = ['above', 'below'];
+
+  positionStartOptions = [...this.positionYOptions, ...this.positionXOptions];
+  positionEndOptions: any[] = [];
+
+  positionStart: MtxPopoverPositionStart = 'after';
+  positionEnd: MtxPopoverPositionEnd = 'below';
+
+  onPositionStartChange(value?: string) {
+    if (this.positionXOptions.includes(this.positionStart)) {
+      this.positionEnd = 'below';
+      this.positionEndOptions = [...this.positionYOptions, 'center'];
+    }
+
+    if (this.positionYOptions.includes(this.positionStart)) {
+      this.positionEnd = 'after';
+      this.positionEndOptions = [...this.positionXOptions, 'center'];
+    }
+  }
+
+  ngOnInit() {
+    this.onPositionStartChange();
+  }
 }
