5508c2dd0595d0d652b6665bbf0dc9fa69ffc66a
nzbin <nzb329@163.com>
2021-12-12 23:48:31 +0800

    chore(datetimepicker): export types

--- projects/extensions/datetimepicker/public-api.ts
+++ projects/extensions/datetimepicker/public-api.ts
@@ -10,3 +10,4 @@ export * from './month-view';
 export * from './year-view';
 export * from './multi-year-view';
 export * from './datetimepicker-animations';
+export * from './datetimepicker-types';
