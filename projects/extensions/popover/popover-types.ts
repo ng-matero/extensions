33069b9fe6a6197006aad87d8ad4e2c0b6ecba1d
nzbin <nzb329@163.com>
2022-10-09 23:01:04 +0800

    refactor(popover): add `elevation` option and small fix

--- projects/extensions/popover/popover-types.ts
+++ projects/extensions/popover/popover-types.ts
@@ -1,9 +1,14 @@
+/** First value of popover's position. */
 export type MtxPopoverPositionStart = 'above' | 'below' | 'before' | 'after';
 
+/** Second value of popover's position. */
 export type MtxPopoverPositionEnd = MtxPopoverPositionStart | 'center';
 
+/** Popover's position. */
 export type MtxPopoverPosition = [MtxPopoverPositionStart, MtxPopoverPositionEnd];
 
+/** Popover's trigger event. */
 export type MtxPopoverTriggerEvent = 'click' | 'hover' | 'none';
 
-export type MtxPopoverScrollStrategy = 'noop' | 'close' | 'block' | 'reposition';
+/** Reason why the popover was closed. */
+export type PopoverCloseReason = void | 'click' | 'keydown' | 'tab';
