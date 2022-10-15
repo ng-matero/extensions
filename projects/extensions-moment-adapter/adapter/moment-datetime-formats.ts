e77cadd65f44a09e883b28de962181d2d5e4119d
nzbin <nzb329@163.com>
2022-10-15 11:49:28 +0800

    fix(extensions-moment-adapter): add `yearInput` format

--- projects/extensions-moment-adapter/adapter/moment-datetime-formats.ts
+++ projects/extensions-moment-adapter/adapter/moment-datetime-formats.ts
@@ -4,12 +4,14 @@ export const MTX_MOMENT_DATETIME_FORMATS: MtxDatetimeFormats = {
   parse: {
     dateInput: 'L',
     monthInput: 'MMMM',
+    yearInput: 'YYYY',
     timeInput: 'LT',
     datetimeInput: 'L LT',
   },
   display: {
     dateInput: 'L',
     monthInput: 'MMMM',
+    yearInput: 'YYYY',
     datetimeInput: 'L LT',
     timeInput: 'LT',
     monthYearLabel: 'MMM YYYY',
