2751df703984dc6e091078af5c04cfab006bea67
nzbin <nzb329@163.com>
2023-02-13 13:35:38 +0800

    build(extensions): fix build errors

--- projects/extensions/core/pipes/index.ts
+++ projects/extensions/core/pipes/index.ts
@@ -1,2 +1,3 @@
 export * from './pipes.module';
 export * from './to-observable.pipe';
+export * from './is-template-ref.pipe';
--- projects/extensions/tooltip/tooltip-module.ts
+++ projects/extensions/tooltip/tooltip-module.ts
@@ -1,15 +1,15 @@
-import { OverlayModule } from '@angular/cdk/overlay';
 import { A11yModule } from '@angular/cdk/a11y';
+import { OverlayModule } from '@angular/cdk/overlay';
+import { CdkScrollableModule } from '@angular/cdk/scrolling';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatCommonModule } from '@angular/material/core';
-import { CdkScrollableModule } from '@angular/cdk/scrolling';
+import { MtxPipesModule } from '@ng-matero/extensions/core';
 import {
   MtxTooltip,
-  TooltipComponent,
   MTX_TOOLTIP_SCROLL_STRATEGY_FACTORY_PROVIDER,
+  TooltipComponent,
 } from './tooltip';
-import { MtxPipesModule } from '../core';
 
 @NgModule({
   imports: [A11yModule, CommonModule, OverlayModule, MatCommonModule, MtxPipesModule],
