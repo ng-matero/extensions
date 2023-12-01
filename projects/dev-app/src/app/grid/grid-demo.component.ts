6c7b5f62ed6a7140c669f5146c83344f885d155e
Marceli <38301040+marcelimati@users.noreply.github.com>
2023-12-01 12:28:10 +0100

    fix: remove unused imports

--- projects/dev-app/src/app/grid/grid-demo.component.ts
+++ projects/dev-app/src/app/grid/grid-demo.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import {
   MtxGrid,
   MtxGridColumn,
