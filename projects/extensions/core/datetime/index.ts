8a97af3bee532c2d0f4f7e3361ac1e89213c7e81
nzbin <nzb329@163.com>
2024-02-06 23:35:05 +0800

    fix(core): fix the missing export `provideNativeDatetimeAdapter`

--- projects/extensions/core/datetime/index.ts
+++ projects/extensions/core/datetime/index.ts
@@ -1,5 +1,5 @@
-export { NativeDatetimeModule, MtxNativeDatetimeModule } from './datetime.module';
-export { DatetimeAdapter } from './datetime-adapter';
-export { MtxDatetimeFormats, MTX_DATETIME_FORMATS } from './datetime-formats';
-export { NativeDatetimeAdapter } from './native-datetime-adapter';
-export { MTX_NATIVE_DATETIME_FORMATS } from './native-datetime-formats';
+export * from './datetime.module';
+export * from './datetime-adapter';
+export * from './datetime-formats';
+export * from './native-datetime-adapter';
+export * from './native-datetime-formats';
