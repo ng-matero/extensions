42466dd4532f6c3a41ce1838b8a5bd2b3aa07344
nzbin <nzb329@163.com>
2024-04-20 16:52:00 +0800

    fix(select): fix `groupBy` type and add `bufferAmount`

--- projects/extensions/select/select.component.ts
+++ projects/extensions/select/select.component.ts
@@ -198,8 +198,9 @@ export class MtxSelectComponent
   @Input() clearOnBackspace = true;
   @Input() compareWith!: CompareWithFn;
   @Input() dropdownPosition: DropdownPosition = 'auto';
-  @Input() groupBy!: string | (() => void);
+  @Input() groupBy!: string | ((value: any) => any);
   @Input() groupValue!: GroupValueFn;
+  @Input() bufferAmount = 4;
   @Input() selectableGroup = false;
   @Input() selectableGroupAsModel = true;
   @Input() hideSelected = false;
