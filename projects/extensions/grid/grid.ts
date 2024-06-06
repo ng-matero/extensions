cfad7da57c1c5f5b8a9acf05f47655e50c130639
nzbin <nzb329@163.com>
2024-06-06 23:24:31 +0800

    chore(grid): remove unused codes

--- projects/extensions/grid/grid.ts
+++ projects/extensions/grid/grid.ts
@@ -565,9 +565,6 @@ export class MtxGrid implements OnChanges, AfterViewInit, OnDestroy {
   selector: '[mtx-grid-selectable-cell]',
 })
 export class MtxGridSelectableCell {
-  private _selected = false;
-  private _rowData!: Record<string, any>;
-
   ctrlKeyPressed = false;
   shiftKeyPressed = false;
 
@@ -575,13 +572,7 @@ export class MtxGridSelectableCell {
   get selected(): boolean {
     return this._selected;
   }
-
-  @Input()
-  set mtxSelectableRowData(value: any) {
-    if (value !== this._rowData) {
-      this._rowData = value;
-    }
-  }
+  private _selected = false;
 
   @Output() cellSelectionChange = new EventEmitter<MtxGridSelectableCell>();
 
