72e2eb01e83f2d04a42919dea03c2ae592c5f0ff
nzbin <nzb329@163.com>
2024-06-07 14:21:00 +0800

    refactor(grid): use dataSource to check empty data

--- projects/extensions/grid/grid.component.ts
+++ projects/extensions/grid/grid.component.ts
@@ -265,7 +265,7 @@ export class MtxGridComponent implements OnChanges, AfterViewInit, OnDestroy {
   @Input() noResultTemplate!: TemplateRef<any>;
 
   get _hasNoResult() {
-    return (!this.data || this.data.length === 0) && !this.loading;
+    return (!this.dataSource.data || this.dataSource.data.length === 0) && !this.loading;
   }
 
   // ===== Cell Templates =====
