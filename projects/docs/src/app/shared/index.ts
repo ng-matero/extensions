8baca78d06e84ece2d01e5527850a4aa10c94f47
nzbin <nzb329@163.com>
2025-06-09 12:44:58 +0800

    fix(docs): remove directionality file

--- projects/docs/src/app/shared/index.ts
+++ projects/docs/src/app/shared/index.ts
@@ -1,2 +1,1 @@
-export * from './directionality';
 export * from './themes';
--- projects/docs/src/main.ts
+++ projects/docs/src/main.ts
@@ -1,11 +1,9 @@
-import { Directionality } from '@angular/cdk/bidi';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { AppComponent } from './app/app.component';
 import { DOCS_APP_ROUTES } from './app/routes';
-import { AppDirectionality } from './app/shared';
 
 bootstrapApplication(AppComponent, {
   providers: [
@@ -15,6 +13,5 @@ bootstrapApplication(AppComponent, {
       DOCS_APP_ROUTES,
       withInMemoryScrolling({ scrollPositionRestoration: 'enabled', anchorScrolling: 'enabled' })
     ),
-    { provide: Directionality, useClass: AppDirectionality },
   ],
 }).catch(err => console.error(err));
